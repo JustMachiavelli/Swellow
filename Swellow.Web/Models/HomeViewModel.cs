35d06a0a069d76efe793b855ba41c88eb40a96a3
JustMachiavelli <1780089183@qq.com>
2021-02-24 19:03:57 +0800

    重写

--- Swellow.Web/Models/HomeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Swellow.Web.Models
-{
-    public class HomeViewModel
-    {
-        public IEnumerable<ModelComponents.LibraryPreview> LibraryPreviews;
-    }
-}
